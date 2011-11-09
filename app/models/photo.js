20787e5c4f24971079ab40857d711b1c5759add7
David Wolf <dpwolf@mac.com>
2011-11-09 18:47:12 -0500

    added parse function and cleaned up photo model

--- app/models/photo.js
+++ app/models/photo.js
@@ -1,50 +1,13 @@
 tripmapper.models.photo = Backbone.Model.extend({
-    // defaults: {
-    //     "username": "", 
-    //     "status": "public", 
-    //     "server_id": "2", 
-    //     "user_id": 0, 
-    //     "description": "", 
-    //     "rating": 5, 
-    //     "spot_image_rating": null, 
-    //     "favorite": false, 
-    //     "comments": 0, 
-    //     "flagged": false, 
-    //     "width": 0, 
-    //     "secret": "", 
-    //     "location": {
-    //         "foursquare_venue_id": null, 
-    //         "longitude": 0, 
-    //         "location": "", 
-    //         "spot_id": null, 
-    //         "latitude": 0, 
-    //         "foursquare_venue_name": null
-    //     }, 
-    //     "date": "", 
-    //     "shared": {
-    //         "tumblr": false, 
-    //         "tumblr_reblog": 0, 
-    //         "foursquare_checkin": false, 
-    //         "facebook_album": false, 
-    //         "facebook_newsfeed": false, 
-    //         "tweeted": false, 
-    //         "retweeted": 0, 
-    //         "facebook_shared": 0
-    //     }, 
-    //     "height": 0, 
-    //     "id": "", 
-    //     "favorite_count": 0
-    // },
-    // initialize: function(){
-    //     console.log('initialize a photo');
-    //     this.bind("change:id",function(){
-    //         var id = this.get("id");
-    //         console.log('changed id to ', id);
-    //     });
-    //     this.bind("change:favorite",function(){
-    //         var fav = this.get("favorite");
-    //         console.log('changed favorite to ', fav);
-    //     });
-    // }
-    
+    urlRoot: tripmapper.api_base + '/photo/',
+    url: function(method){
+        return this.urlRoot;
+    },
+    parse: function(d,xhr){
+        if(d.response && d.response.photos){
+            return d.response.photos[0];
+        }else{
+            return {};
+        }
+    }
 });
