b9d8b8bcacf2fb401d1fde00d60c791e513d5fa7
David Wolf <dpwolf@mac.com>
2011-11-09 18:42:18 -0500

    added favorite model added it to index changed template from 'like' to 'favorite'

---
+++ app/models/favorite.js
@@ -0,0 +1,18 @@
+tripmapper.models.favorite = Backbone.Model.extend({
+    urlRoot: tripmapper.api_base + '/favorite/',
+    url: function(method){
+        if(method){
+            switch(method){
+                case 'delete':
+                    return this.urlRoot + 'remove/';
+                // since backbone treats any model that has an id as not 'new' 
+                // it will actually trigger an 'update' method to post a favorite
+                // default will catch this for us
+                default:
+                    return this.urlRoot;
+            }
+        }else{
+            return this.urlRoot;
+        }
+    }
+});
