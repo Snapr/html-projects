7f037d142128fe5a1831f24639e80423e3228d41
David Wolf <dpwolf@mac.com>
2011-11-09 15:33:36 -0500

    added comment model

---
+++ app/models/comment.js
@@ -0,0 +1,21 @@
+tripmapper.models.comment = Backbone.Model.extend({
+    urlRoot: tripmapper.api_base + '/comment/',
+    url: function(method){
+        if(method){
+            switch(method){
+                case 'create':
+                    return this.urlRoot;
+                case 'update':
+                    return this.urlRoot + 'edit/';
+                case 'delete':
+                    return this.urlRoot + 'delete/';
+                default:
+                    return this.urlRoot;
+            }
+        }else{
+            return this.urlRoot;
+        }
+
+    },
+
+});
