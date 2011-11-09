ac513f15f19397b78618a8a7a6cf67956da7e9e9
David Wolf <dpwolf@mac.com>
2011-11-09 15:41:06 -0500

    moved template in thumbs_li view

--- app/views/thumbs_li.js
+++ app/views/thumbs_li.js
@@ -2,11 +2,16 @@ tripmapper.views.thumbs_li = Backbone.View.extend({
     initialize: function(){
         console.log('initialise thumbs_li view');
     },
+    template: _.template( $("#thumb-li-template").html() ),
     render: function(callback){
         var el = this.el.empty();
-        var compiled_template = _.template( $("#thumb-li-template").html() );
-        el.html( compiled_template( { results: this.collection.models } ) ).listview().listview("refresh");
-        if(callback){
+        _.each(this.collection.models,function(item){
+            // console.warn(item.get('id'));
+        });
+        var results = this.collection.models;
+        el.html( this.template( { results: results } ) )
+        // .listview().listview("refresh");
+        if(callback && typeof callback == 'function'){
             callback();
         }
     }
