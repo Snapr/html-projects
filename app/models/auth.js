d3d50b866fe64781e44fe63d1252d98b11bdfb5a
David Wolf <dpwolf@mac.com>
2011-11-17 19:40:26 -0500

    added get_token method to auth model

--- app/models/auth.js
+++ app/models/auth.js
@@ -1,2 +1,42 @@
 tripmapper.models.auth = Backbone.Model.extend({
+    url:function(){
+        return tripmapper.access_token_url;
+    },
+    get_token: function(username,password,options){
+        this.data = {
+            'grant_type':'password',
+            'client_id':tripmapper.client_id,
+            'client_secret':tripmapper.client_secret,
+            'username': username,
+            'password': password,
+            '_method':'POST'
+        }
+        var _this = this;
+        var opt = {
+            success: function(response){
+                console.warn('response',response,_this)
+                if(_this.get("access_token")){
+                    _this.set({username:username});
+                    delete _this.data;
+                    if(typeof options.success == 'function'){
+                        console.warn('success == function');
+                        options.success();
+                    }
+                }else{
+                    delete _this.data;
+                    if(typeof options.error == 'function'){
+                        console.warn('error == function');
+                        options.error(_this.get('error_description'));
+                    }
+                }
+            },
+            error: function(){
+                if(typeof options.error == 'function'){
+                    console.warn('error == function');
+                    options.error();
+                }
+            }
+        }
+        this.fetch(opt);
+    }
 });
