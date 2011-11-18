e0a2783b441a68ba80fb77ff9ca0f63b97f6b2e6
David Wolf <dpwolf@mac.com>
2011-11-17 19:42:49 -0500

    added functions to join_snapr view

--- app/views/join_snapr.js
+++ app/views/join_snapr.js
@@ -1,9 +1,54 @@
 tripmapper.views.join_snapr = Backbone.View.extend({
     el:$('#join-snapr'),
+    events:{
+        "submit #join-dialog":"join"
+    },
     initialize:function(){
         $.mobile.changePage($("#join-snapr"),{changeHash:false});
     },
     render:function(){
+    },
+    join:function(){
+        var new_user = new tripmapper.models.user;
+        new_user.data = {
+           username: this.el.find("#join-dialog-username").val(),
+           password: this.el.find("#join-dialog-password").val(),
+           email: this.el.find("#join-dialog-email").val(),
+           client_id: tripmapper.client_id
+        }
+        
+        _this = this;
+        
+        // these options will be triggered on login (after successful join)
+        var login_options = {
+            success:function(){
+                // empty all the forms
+                _this.el.find("#join-dialog-username").val('');
+                _this.el.find("#join-dialog-password").val('');
+                _this.el.find("#join-dialog-email").val('')
+                // go back to home screen
+                Route.navigate('#',true);
+            },
+            error:function(){
+                console.warn('error on login after successful join');
+            }
+        }
+        // these options will be triggered on join
+        var join_options = {
+            success:function(s){
+                if(s.get('success')){
+                    tripmapper.auth.get_token(new_user.data.username,new_user.data.password,login_options);
+                }else{
+                    alert(s.get('error').message);
+                    console.warn(s.get('error').message);
+                }
+            },
+            error:function(e){
+                console.warn('error',e);
+            }
+        }
+        // save creates a new user
+        new_user.save({},join_options);
     }
 });
 
