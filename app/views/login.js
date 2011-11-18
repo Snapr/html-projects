280de3a81bada2c2e77d5eada7eb7a841dd624c2
David Wolf <dpwolf@mac.com>
2011-11-17 19:43:07 -0500

    reworked login view

--- app/views/login.js
+++ app/views/login.js
@@ -1,7 +1,7 @@
 tripmapper.views.login = Backbone.View.extend({
     el: $('#login'),
     events: {
-        "submit #login-dialog":"get_auth_token"
+        "submit #login-dialog":"log_in"
     },
     initialize: function(){
         // console.warn('init login')
@@ -9,54 +9,27 @@ tripmapper.views.login = Backbone.View.extend({
         if(!tripmapper.auth){
             // console.warn('no auth')
             tripmapper.auth = new tripmapper.models.auth;
-            tripmapper.auth.url = tripmapper.access_token_url;
         }
     },
-    get_auth_token: function(){
+    log_in: function(){
         // console.warn('get_auth_token')
         var username = $("#login-dialog-username").val();
         var password = $("#login-dialog-password").val();
-        if(tripmapper.auth){
-            tripmapper.auth.data = {
-                    'grant_type':'password',
-                    'client_id':tripmapper.client_id,
-                    'client_secret':tripmapper.client_secret,
-                    'username': username,
-                    'password': password,
-                    '_method':'POST'
-            }
-            // tripmapper.auth.bind('change:username',function(){
-            //     console.warn('reset username');
-            // });
-            // tripmapper.auth.bind('change:access_token',function(){
-            //     console.warn('reset access_token');
-            // });
-            var options = {
-                success: function(response){
-                    if(tripmapper.auth.get("access_token")){
-                        console.warn("access_token", tripmapper.auth.get("access_token"));
-                        delete tripmapper.auth.data;
-                        $("#login-dialog-username").val('');
-                        $("#login-dialog-password").val('');
-                        tripmapper.auth.set({username:username});
-                        if(window.location.hash == "#login"){
-                            window.history.back();
-                        }else{
-                            Route.navigate(window.location.hash,true);
-                        }
-                    }else{
-                        console.warn('response',response);
-                        delete tripmapper.auth.data;
-                        alert(response.get("error_description") || "Sorry, we had trouble logging in. Please try again.");
-                    }
-                },
-                error: function(error){
-                    console.warn('error',error);
+
+        var options = {
+            success: function(response){
+                $("#login-dialog-username").val('');
+                $("#login-dialog-password").val('');
+                if(window.location.hash == "#login"){
+                    window.history.back();
+                }else{
+                    Route.navigate(window.location.hash,true);
                 }
+            },
+            error: function(error){
+                alert(error || "Sorry, we had trouble logging in. Please try again.");
             }
-            tripmapper.auth.fetch(options);
-        }else{
-            console.warn('no auth')
         }
+        tripmapper.auth.get_token(username,password,options);
     }
 });
