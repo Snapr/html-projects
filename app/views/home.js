f102fc7165de6655a49130902cdab79a57f323f9
David Wolf <dpwolf@mac.com>
2011-11-17 19:41:55 -0500

    reworked home view to set el on intitialization reset button names/functions on home screen when the auth username changes

--- app/views/home.js
+++ app/views/home.js
@@ -1,32 +1,37 @@
 tripmapper.views.home = Backbone.View.extend({
-    el:$('#menu'),
     initialize:function(){
-        var login_logout = new tripmapper.views.login_logout({model: tripmapper.auth});
-        login_logout.render();
-        var _this = this;
-        tripmapper.auth.bind("change:username",function(){
-            login_logout.model = tripmapper.auth;
-            login_logout.render();
-            _this.render();
-        });
         if($.mobile.activePage && $.mobile.activePage.find("#menu").length < 1){
             console.warn('changing page');
             $.mobile.changePage("#menu");
         }
+        this.el = $('#menu');
         window.location.hash = "";
-        this.render();
+        var _this = this;
+        tripmapper.auth.bind("change:username",function(){
+            console.warn('change:username')
+            _this.render();
+        });
+        tripmapper.auth.change();
     },
     render:function(){
-        if(tripmapper.auth.get("access_token")){
+        // this is kinda messy. need to move it to a template
+        if(tripmapper.auth.get("username")){
             this.el.find('a.my-snaps .ui-btn-text').text('My Snaps');
             this.el.find('a.my-snaps').attr('href',"#feed/?username="+tripmapper.auth.get("username").toLowerCase());
+            this.el.find('a.login-logout .ui-btn-text').text('Log Out');
+            this.el.find('a.login-logout').attr('href',"#logout");
+
+            this.el.find('a.join-account .ui-btn-text').text('My Account');
+            this.el.find('a.join-account').attr('href',"#account");
+
         }else{
             this.el.find('a.my-snaps .ui-btn-text').text('Latest Photos');
             this.el.find('a.my-snaps').attr('href',"#feed");
+            this.el.find('a.login-logout .ui-btn-text').text('Log In')
+            this.el.find('a.login-logout').attr('href',"#login");
+
+            this.el.find('a.join-account .ui-btn-text').text('Join')
+            this.el.find('a.join-account').attr('href',"#join");
         }
     }
 });
-
-
-
-
