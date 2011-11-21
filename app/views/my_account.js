84e1e60006bc47507af672e2d64659ba28017406
David Wolf <dpwolf@mac.com>
2011-11-21 18:36:55 -0500

    get user_settings for the current user when they go to my-account

--- app/views/my_account.js
+++ app/views/my_account.js
@@ -1,5 +1,18 @@
 tripmapper.views.my_account = Backbone.View.extend({
     initialize:function(){
         $.mobile.changePage($("#my-account"),{changeHash:false});
+        this.user_settings = new tripmapper.models.user_settings;
+        var _this = this;
+        var options = {
+            success:function(){
+                _this.user_settings.linked_services_setup();
+                console.warn('success',_this.user_settings);
+                console.warn('linked_services',_this.user_settings.get('linked_services'));
+            },
+            error:function(){
+                console.warn('error',_this);
+            }
+        }
+        this.user_settings.fetch(options);
     }
 });
