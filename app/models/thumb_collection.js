f771cef9054ca25aa3756ab87cc248723a4221c0
David Wolf <dpwolf@mac.com>
2011-11-22 20:32:39 -0500

    set up parsing for thumb_collection

--- app/models/thumb_collection.js
+++ app/models/thumb_collection.js
@@ -1,3 +1,12 @@
-tripmapper.models.thumb_collection = Backbone.Model.extend({
-    model:tripmapper.models.thumb
+tripmapper.models.thumb_collection = Backbone.Collection.extend({
+    model:tripmapper.models.thumb,
+    urlRoot: tripmapper.api_base + '/thumbs/',
+    url: function(method){
+        return this.urlRoot;
+    },
+    parse: function(d,xhr){
+        if(d.success && d.response && d.response.photos){
+            return d.response.photos;
+        }
+    }
 });
