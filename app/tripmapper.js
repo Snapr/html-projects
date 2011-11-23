13e29cc26ce45f91310d6bbe7302202e60b9d638
David Wolf <dpwolf@mac.com>
2011-11-22 20:35:58 -0500

    added SnaprOverlay for making map thumbs

--- app/tripmapper.js
+++ app/tripmapper.js
@@ -183,6 +183,93 @@ tripmapper.routers = Backbone.Router.extend({
 });
 
 $(function(){
+    tripmapper.SnapOverlay = function(type, data, map, extra_class){
+        // image as JS object in format the snapr api returns
+        this.type_ = type;
+        this.data_ = data;
+        this.map_ = map;
+        this.extra_class_ = extra_class;
+
+        // We define a property to hold the image's
+        // div. We'll actually create this div
+        // upon receipt of the add() method so we'll
+        // leave it null for now.
+        this.div_ = null;
+
+        // Explicitly call setMap() on this overlay
+        this.setMap(map);
+    }
+
+    tripmapper.SnapOverlay.prototype = new google.maps.OverlayView();
+    tripmapper.SnapOverlay.prototype.onAdd = function() {
+        // Note: an overlay's receipt of onAdd() indicates that
+        // the map's panes are now available for attaching
+        // the overlay to the map via the DOM.
+
+        var data_id = this.data_.id;
+
+        if (this.type_ == 'photo') {
+            var div = $(_this.thumb_template({photo:this.data_}));
+            div.show();
+        } else {  //spot
+            var div = $(_this.spot_template({spot:this.data_}));
+            div.show();
+        }
+
+        // Set the overlay's div_ property to this DIV
+        this.div_ = div;
+
+        // We add an overlay to a map via one of the map's panes.
+        // We'll add this overlay to the overlayImage pane.
+        var panes = this.getPanes();
+        $(panes.floatPane).append(this.div_);
+    }
+
+    tripmapper.SnapOverlay.prototype.draw = function() {
+        var overlayProjection = this.getProjection();
+        var position = new google.maps.LatLng( this.data_.location.latitude, this.data_.location.longitude );
+        var px = overlayProjection.fromLatLngToDivPixel( position );
+
+        this.div_ = this.div_
+            .css('position', 'absolute')
+            .css('left', px.x + 'px')
+            .css('top', px.y + 'px');
+    }
+
+    tripmapper.SnapOverlay.prototype.onRemove = function() {
+        this.div_.parentNode.removeChild(this.div_);
+        this.div_ = null;
+    }
+    tripmapper.SnapOverlay.prototype.hide = function() {
+        if (this.div_) {
+          this.div_.style.visibility = "hidden";
+        }
+    }
+
+    tripmapper.SnapOverlay.prototype.show = function() {
+        if (this.div_) {
+          this.div_.style.visibility = "visible";
+        }
+    }
+
+    tripmapper.SnapOverlay.prototype.toggle = function() {
+        if (this.div_) {
+          if (this.div_.style.visibility == "hidden") {
+            this.show();
+          } else {
+            this.hide();
+          }
+        }
+    }
+
+    tripmapper.SnapOverlay.prototype.toggleDOM = function() {
+        if (this.getMap()) {
+          this.setMap(null);
+        } else {
+          this.setMap(this.map_);
+        }
+    }
+
     // initialise router and start backbone
     Route = new tripmapper.routers;
     Backbone.history.start();
