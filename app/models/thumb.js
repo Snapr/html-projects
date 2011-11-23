2144a72210d076cb7174c514a8fbddbb2b09d2b3
David Wolf <dpwolf@mac.com>
2011-11-22 20:32:22 -0500

    clean up thumb model

--- app/models/thumb.js
+++ app/models/thumb.js
@@ -1,18 +1,4 @@
 tripmapper.models.thumb = Backbone.Model.extend({
-    defaults: {
-        "username": "", 
-        "width": 0, 
-        "secret": "", 
-        "location": {
-            "latitude": 0,
-            "longitude": 0
-        }, 
-        "date": "", 
-        "description": "", 
-        "server_id": "", 
-        "id": "", 
-        "height": 0
-    },
     initialize: function(){
         console.log('initialize a thumb');
     }
