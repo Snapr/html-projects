121dedbc4073bf6aacf3296952378ee6f651b3e5
David Wolf <dpwolf@mac.com>
2011-12-02 17:30:49 -0500

    switched to using navbar for selecting timeframe in popular view

--- app/views/popular.js
+++ app/views/popular.js
@@ -1,12 +1,19 @@
 tripmapper.views.popular = Backbone.View.extend({
+
     el: $('#popular'),
+
     events: {
-        "change #popular-timeframe":"update_list"
+        "click #popular-timeframe a":"update_list"
     },
-    initialize: function(){
-        console.warn('init pop')
-        $.mobile.changePage($("#popular"),{changeHash:false});
-        this.photo_collection = new tripmapper.models.photo_collection;
+
+    initialize: function()
+    {
+        console.warn('init pop');
+
+        $.mobile.changePage( $("#popular"), {
+            changeHash: false
+        });
+        this.photo_collection = new tripmapper.models.photo_collection();
         this.photo_collection.url = tripmapper.api_base + "/search/";
         this.photo_collection.data = {
             sort:"favorite_count",
@@ -14,53 +21,62 @@ tripmapper.views.popular = Backbone.View.extend({
         }
         this.update_list();
     },
-    update_list: function(){
-        var _this = this;
-        console.log('render popular view',_this);
-        var time = $('input:radio[name=options-popular-timeframe]:checked').val();
-        console.log('popular',time);
-        switch(time){
+
+    update_list: function( e )
+    {
+        var time = e && e.currentTarget.id.replace( 'popular-', '' ) || 'time-all';
+
+        var popuar_view = this;
+        
+        switch (time){
             case 'time-today':
                 var today = new Date();
-                _this.photo_collection.data.min_date = tripmapper.utils.date_to_snapr_format(today);
+                popuar_view.photo_collection.data.min_date = tripmapper.utils.date_to_snapr_format( today );
                 break;
             case 'time-week':
                 var day = new Date();
-                day.setMilliseconds(day.getMilliseconds() - (7*24*60*60*1000));
-                _this.photo_collection.data.min_date = tripmapper.utils.date_to_snapr_format(day);
+                day.setMilliseconds( day.getMilliseconds() - (7*24*60*60*1000) );
+                popuar_view.photo_collection.data.min_date = tripmapper.utils.date_to_snapr_format( day );
                 break;
             case 'time-all':
-                if(_this.photo_collection.data.min_date){
-                    console.warn('delete')
-                    delete _this.photo_collection.data.min_date;
+                if(popuar_view.photo_collection.data.min_date){
+                    delete popuar_view.photo_collection.data.min_date;
                 }
         }
+        
         var options = {
-            success:function(){
-                console.warn('success')
+            success: function()
+            {
+                console.warn('success');
                 popular_list = new tripmapper.views.thumbs_li({
-                    collection:_this.photo_collection,
-                    el:$('#popular ul').eq(0)
+                    collection: popuar_view.photo_collection,
+                    el: $('#popular ul.grid-list').eq(0)
                 });
-                popular_list.render(function(){
+                popular_list.render( function()
+                {
                     $.mobile.hidePageLoadingMsg();
                     // store the last query
                     console.warn('store the last query');
-                    $('#popular ul').eq(0).data('query',{time:time,auth:tripmapper.auth});
+                    $('#popular ul.grid-list').eq(0).data('query', {
+                        time: time,
+                        auth: tripmapper.auth
+                    });
                 });
             },
-            error:function(){
+            error:function()
+            {
                 console.warn('error');
                 $.mobile.hidePageLoadingMsg();
             }
         }
-
+        
         // only update list if the query has changed or is new
-        if(!_.isEqual($('#popular ul').eq(0).data('query'), {time:time,auth:tripmapper.auth}) ){
+        if (!_.isEqual( $('#popular ul.grid-list').eq(0).data('query'), {time: time, auth: tripmapper.auth} ))
+        {
             console.warn('loading');
             $.mobile.loadingMessage = "Loading popular photos";
             $.mobile.showPageLoadingMsg();
-            _this.photo_collection.fetch(options);
+            popuar_view.photo_collection.fetch( options );
         }
 
     }
