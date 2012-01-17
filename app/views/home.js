5327600c97dc21a2566ebed3055ce7d1af243781
David Wolf <dpwolf@mac.com>
2012-01-17 16:25:12 +1100

    if in appmode use snapr://camera and snapr://camera-roll from home screen

--- app/views/home.js
+++ app/views/home.js
@@ -14,48 +14,39 @@ snapr.views.home = Backbone.View.extend({
             console.warn( 'changing page' );
             $.mobile.changePage( "#home" );
         }
-
-        // var home_view = this;
-        // snapr.auth.bind( "set:username", function()
-        // {
-        //     console.warn( 'set:username' )
-        //     home_view.render();
-        // });
-        // snapr.auth.bind( "unset:username" , function()
-        // {
-        //     console.warn( 'unset:username' )
-        //     home_view.render();
-        // });
-        // 
-        // snapr.auth.change();
-        // this.render();
+    },
+    
+    events: {
+        "click .camera": "camera",
+        "click .camera-roll": "camera_roll"
+    },
+    
+    camera: function()
+    {
+        console.warn("camera");
+        if (snapr.utils.get_local_param("appmode"))
+        {
+            pass_data("snapr://camera");
+        }
+        else
+        {
+            Route.navigate( '#/upload/', true );
+        }
     },
 
-    // template: _.template( $('#home-template').html() ),
+    camera_roll: function()
+    {
+        console.warn("camera-roll");
+        if (snapr.utils.get_local_param("appmode"))
+        {
+            pass_data("snapr://camera-roll");
+        }
+        else
+        {
+            Route.navigate( '#/upload/', true );
+        }
 
-    // render: function()
-    // {
-    //     console.warn( 'render home' )
-    //     if (snapr.auth && snapr.auth.attributes.snapr_user)
-    //     {
-    //         var logged_in = true;
-    //         var snapr_user = snapr.auth.attributes.snapr_user;
-    //         
-    //     }
-    //     else
-    //     {
-    //         var logged_in = false;
-    //         var snapr_user = null;
-    //     }
-    //     this.el.find( '[data-role="content"]' )
-    //         .replaceWith(
-    //             $(this.template( {
-    //                 logged_in: logged_in,
-    //                 username: snapr_user,
-    //                 appmode: snapr.utils.get_local_param("appmode")
-    //             })));
-    //     this.el.trigger( "create" );
-    //     
-    //     return this;
-    // }
+    }
+
+
 });
