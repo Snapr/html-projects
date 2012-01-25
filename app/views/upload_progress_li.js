4f9e2a9088c3a30ea4de8b76ce8b4cbe66853f0c
David Wolf <dpwolf@mac.com>
2012-01-26 10:28:52 +1100

    upload progress spinner added

--- app/views/upload_progress_li.js
+++ app/views/upload_progress_li.js
@@ -14,14 +14,30 @@ snapr.views.upload_progress_li = Backbone.View.extend({
     
     render: function()
     {
+        // check that the progress hasn't already reached 100%
+        if (!$(this.el).find(".finishing").length)
+        {
+            $(this.el).html( 
+                this.template({
+                    upload_status: this.photo.upload_status,
+                    img_url: this.photo.thumbnail,
+                    percent_complete: this.photo.percent_complete
+                })
+            );
 
-        $(this.el).html( 
-            this.template({
-                upload_status: this.photo.upload_status,
-                img_url: this.photo.thumbnail,
-                percent_complete: this.photo.percent_complete
-            })
-        );
+            if (this.photo.percent_complete == 100)
+            {
+                $(this.el).find(".finishing").spin({
+                    lines:10,
+                    length:3,
+                    width:2,
+                    radius:3,
+                    trail:50,
+                    speed:1.0,
+                    color:'#000000'
+                });
+            }
+        }
 
         return this;
     },
