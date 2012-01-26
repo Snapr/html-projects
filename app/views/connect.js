13a44056384f03e66050bc4ecdc1d5ea3c45b6e9
David Wolf <dpwolf@mac.com>
2012-01-26 19:28:59 +1100

    oops, added connect view files

---
+++ app/views/connect.js
@@ -0,0 +1,106 @@
+snapr.views.connect = Backbone.View.extend({
+
+    initialize: function()
+    {
+        _.bindAll( this );
+        this.el.live('pagehide', function( e )
+        {
+            $(e.target).undelegate();
+
+            return true;
+        });
+        
+        $.mobile.changePage( $("#connect"), {
+            changeHash: false
+        });
+
+        this.photo_id = this.options.query && this.options.query.photo_id || null;
+        this.to_link = this.options.query && this.options.query.to_link.split(",") || [];
+        this.shared = this.options.query && this.options.query.shared.split(",") || [];
+        this.to_share = [];
+        if (this.to_link.length && this.options.query && this.options.query.url)
+        {
+            _.each(this.to_link, function(service)
+            {
+                // is the service provider mentioned in the return url
+                if (this.options.query.url.indexOf(service) > -1)
+                {
+                    this.to_share.push( service );
+                }
+            }, this);
+        }
+        
+        this.services = [
+            "facebook",
+            "tumblr"
+        ];
+        
+        this.render();
+        // this.user_settings = new snapr.models.user_settings();
+        // 
+        // var connect_view = this;
+        // var options = {
+        //     success: function()
+        //     {
+        //         connect_view.user_settings.linked_services_setup();
+        //         connect_view.render();
+        //     },
+        //     error: function()
+        //     {
+        //         console.warn( 'error');
+        //     }
+        // }
+        // 
+        // this.user_settings.fetch(options);
+    },
+    
+    render: function()
+    {
+        $(this.el).find("ul").empty();
+        
+        _.each(this.services, function( service )
+        {
+            if (_.indexOf( this.to_share, service ) > -1)
+            {
+                // this service has been linked and can now share
+                // the li will do the sharing itself and redirect
+                // when ready
+                var li = new snapr.views.connect_li({
+                    provider: service,
+                    status: "ready",
+                    photo_id: this.photo_id
+                });
+                $(this.el).find("ul").append( li.render().el );
+            }
+            else if (_.indexOf( this.to_link, service ) > -1)
+            {
+                // this service was not linked
+                var li = new snapr.views.connect_li({
+                    provider: service,
+                    status: "unlinked",
+                    photo_id: this.photo_id
+                });
+                $(this.el).find("ul").append( li.render().el );
+            }
+            else if (_.indexOf( this.shared, service ) > -1)
+            {
+                // this service was shared
+                var li = new snapr.views.connect_li({
+                    provider: service,
+                    status: "shared",
+                    photo_id: this.photo_id
+                });
+                $(this.el).find("ul").append( li.render().el );
+            }
+
+            
+        }, this);
+        
+        $(this.el).find("ul").trigger('create').listview().listview("refresh");
+        console.warn( "render connect", this );
+        // $(this.el).trigger("refresh");
+        
+        return this;
+    }
+
+});
---
+++ app/views/connect_li.js
@@ -0,0 +1,118 @@
+snapr.views.connect_li = Backbone.View.extend({
+    
+    tagName: "li",
+    
+    initialize: function()
+    {
+        _.bindAll( this );
+        this.template = _.template( $("#connect-li-template").html() );
+        this.provider = this.options.provider || null;
+        this.status = this.options.status || null;
+        this.photo_id = this.options.photo_id || null;
+
+        if (this.photo_id && this.status == "ready")
+        {
+            this.share();
+        }
+    },
+    
+    render: function()
+    {
+        $(this.el)
+            .attr("data-role", "fieldcontain")
+            .html( this.template({
+                provider: this.provider,
+                status: this.status
+            }))
+            .trigger("create")
+
+        return this;
+    },
+    
+    events: {
+        "submit .tumblr-login": "link_service",
+        "click .connect": "link_service"
+    },
+    
+    link_service: function()
+    {
+        if (this.provider == "tumblr")
+        {
+            var connect_li = this;
+            $.ajax({
+                url: snapr.api_base + '/linked_services/tumblr/',
+                type: 'POST',
+                dataType: 'jsonp',
+                data:{
+                    username: $('#connect-tumblr-email').val(),
+                    password: $('#connect-tumblr-password').val(),
+                    access_token: snapr.auth.get("access_token"),
+                    _method: "POST"
+                },
+                success: function( data )
+                {
+                    if( data.success )
+                    {
+                        connect_li.status = "ready";
+                        connect_li.render();
+                        connect_li.share();
+                        
+                    }else{
+                        alert( data.error.message );
+                    }
+                },
+                error: function( data )
+                {
+                    console.warn('ajax error!');
+                },
+            });
+            
+        }
+        else
+        {
+            url = snapr.api_base + "/linked_services/"
+                + this.provider + "/oauth/?access_token=" + snapr.auth.get("access_token") + "&redirect=" + escape( window.location.href );
+            window.location = url;
+        }
+        
+    },
+    
+    share: function()
+    {
+        this.model = new snapr.models.photo({id: this.photo_id});
+
+        var connect_li = this;
+
+        var options = {
+            success: function( model, xhr )
+            {
+                console.warn("share success", model, xhr);
+                connect_li.status = "shared";
+                connect_li.render();
+                setTimeout(function()
+                {
+                    Route.navigate("#/love-it/?shared=true&photo_id=" + model.get("id"), true);
+                }, 600);
+            },
+            error: function( error )
+            {
+                console.warn("share error", error);
+            }
+        }
+
+        switch (this.provider)
+        {
+            case "facebook":
+                this.model.save({
+                    facebook_feed: true
+                }, options);
+                break;
+            case "tumblr":
+                this.model.save({
+                    tumblr: true
+                }, options);
+                break;
+        }
+    }
+    
+});
