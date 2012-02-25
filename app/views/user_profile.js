e49220ee1d36835b5e3bd5499c99c226f5234c9d
David Wolf <dpwolf@mac.com>
2012-02-25 16:28:14 -0500

    commented out lines which will be replaced with a template in user profile

--- app/views/user_profile.js
+++ app/views/user_profile.js
@@ -1,26 +1,24 @@
 snapr.views.user_profile = Backbone.View.extend({
-    
+
     initialize: function()
     {
         this.el.live('pagehide', function( e )
         {
             $(e.target).undelegate();
-            
+
             return true;
         });
-
-        this.el.find("h1").text('Username');
-        this.el.find("[data-role='content']").empty();
-        
+        //this.el.find("h1").text('Username');
+        //this.el.find("[data-role='content']").empty();
         this.model = new snapr.models.user( {username: this.options.query.username} );
-        
+
         var user_profile = this;
-        
+
         this.model.bind( "change", function()
         {
             user_profile.render();
         });
-        
+
         // if we are coming from the map view do a flip, otherwise do a slide transition
         if ($.mobile.activePage.attr('id') == 'map' )
         {
@@ -30,20 +28,20 @@ snapr.views.user_profile = Backbone.View.extend({
         {
             var transition = "slidedown";
         }
-        
+
         $.mobile.changePage( $("#user-profile"), {
             changeHash: false,
             transition: transition
         });
-        
+
         this.model.fetch();
     },
-    
+
     render: function()
     {
         console.warn('render', this)
-        this.el.find("h1").text(this.model.get("user").username);
-        this.el.find("[data-role='content']").append(this.model.get("details").profile.bio);
+        //this.el.find("h1").text(this.model.get("user").username);
+        //this.el.find("[data-role='content']").append(this.model.get("details").profile.bio);
     }
-    
+
 });
