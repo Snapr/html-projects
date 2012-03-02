9a0e18f10ff026d94c25466c5142985af6a657d3
David Wolf <dpwolf@mac.com>
2012-03-01 22:05:55 -0500

    venue icons

--- app/views/venue_li.js
+++ app/views/venue_li.js
@@ -12,50 +12,23 @@ snapr.views.venue_li = Backbone.View.extend({
 
     tagName: 'li',
 
+    events: {
+    },
+
     render: function()
     {
+        var icon = this.model.get( "categories" ).length &&
+            this.model.get( "categories" )[0].icon ||
+            'http://foursquare.com/img/categories/none_64.png';
+
+
         $(this.el)
             .empty()
             .append( this.template({
-                venue: this.model
-            }) )
+                venue: this.model,
+                icon: icon
+            }) );
 
         return this;
     },
-
-    refresh: function()
-    {
-        // unfortunately jquery mobile doesn't like refreshing inividual listview items
-        // when the listview has already been created so we need to change things manually
-
-        // var follow_button = $(this.el).find("a[data-icon]");
-        // var following = this.model.get("relationship").you_follow;
-        // if (following)
-        // {
-        //     follow_button
-        //         .attr("data-icon", "check")
-        //         .removeClass("follow")
-        //         .addClass("unfollow")
-        //         .find("span.ui-icon")
-        //             .removeClass("ui-icon-plus")
-        //             .addClass("ui-icon-check")
-        // }
-        // else
-        // {
-        //     follow_button
-        //         .attr("data-icon", "plus")
-        //         .removeClass("unfollow")
-        //         .addClass("follow")
-        //         .find("span.ui-icon")
-        //             .removeClass("ui-icon-check")
-        //             .addClass("ui-icon-plus")
-        // }
-
-    },
-
-    events: {
-        // "click .follow": "follow",
-        // "click .unfollow": "unfollow"
-    },
-
 });
