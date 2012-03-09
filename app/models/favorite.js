4eed92115299c67664deb1f5aec1624d6b3f8c60
David Wolf <dpwolf@mac.com>
2012-03-09 18:42:31 -0500

    fixed model for favorites

--- app/models/favorite.js
+++ app/models/favorite.js
@@ -1,18 +1,21 @@
 snapr.models.favorite = Backbone.Model.extend({
-    //urlRoot: snapr.api_base + '/favorite/',
+    urlRoot: function()
+    {
+        return snapr.api_base + '/favorite/';
+    },
     url: function(method){
         if(method){
             switch(method){
                 case 'delete':
-                    return snapr.api_base + '/favorite/remove/';
+                    return this.urlRoot() + 'remove/';
                 // since backbone treats any model that has an id as not 'new'
                 // it will actually trigger an 'update' method to post a favorite
                 // default will catch this for us
                 default:
-                    return snapr.api_base;
+                    return this.urlRoot();
             }
         }else{
-            return snapr.api_base;
+            return this.urlRoot();;
         }
     }
 });
