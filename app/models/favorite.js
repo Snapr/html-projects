e3b92fa16724dae0fb093cebaaea455ccbe0efc1
David Wolf <dpwolf@mac.com>
2012-03-09 18:46:29 -0500

    fixed dodgy merge

--- app/models/favorite.js
+++ app/models/favorite.js
@@ -3,18 +3,12 @@ snapr.models.favorite = Backbone.Model.extend({
     {
         return snapr.api_base + '/favorite/';
     },
-<<<<<<< HEAD
     url: function( method )
     {
         if (method)
         {
             switch( method )
             {
-=======
-    url: function(method){
-        if(method){
-            switch(method){
->>>>>>> 4eed92115299c67664deb1f5aec1624d6b3f8c60
                 case 'delete':
                     return this.urlRoot() + 'remove/';
                 // since backbone treats any model that has an id as not 'new'
@@ -23,15 +17,10 @@ snapr.models.favorite = Backbone.Model.extend({
                 default:
                     return this.urlRoot();
             }
-<<<<<<< HEAD
         }
         else
         {
             return this.urlRoot();
-=======
-        }else{
-            return this.urlRoot();;
->>>>>>> 4eed92115299c67664deb1f5aec1624d6b3f8c60
         }
     }
 });
