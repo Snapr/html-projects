04500e621cb1191952fad2b4b7fc888a2d318354
David Wolf <dpwolf@mac.com>
2012-03-16 17:57:36 -0400

    added comparator function on reaction collection to keep sored by date

--- app/models/reaction_collection.js
+++ app/models/reaction_collection.js
@@ -1,15 +1,22 @@
 snapr.models.reaction_collection = Backbone.Collection.extend({
     model:snapr.models.reaction,
-    url: function( method ){
+    url: function( method )
+    {
         return snapr.api_base + '/reaction/';
     },
-    parse: function( d, xhr ){
-        if (d.response && d.response.reactions){
+    parse: function( d, xhr )
+    {
+        if (d.response && d.response.reactions)
+        {
             return d.response.reactions;
         }
         else
         {
             return [];
         }
+    },
+    comparator: function( reaction )
+    {
+        return new Date( reaction.get( "date" ) ).getTime();
     }
 });
