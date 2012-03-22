4b7fd6de991923d3e613195b2af41864da6c614d
David Wolf <dpwolf@mac.com>
2012-03-22 15:55:15 -0400

    added render function to map_controls to update based on changes to map query

--- app/views/map_controls.js
+++ app/views/map_controls.js
@@ -7,14 +7,11 @@ snapr.views.map_controls = Backbone.View.extend({
         "click #map-keyword .ui-input-clear": "clear_keyword_search",
     },
 
-    initialize: function () {
+    initialize: function()
+    {
         _.bindAll( this );
         this.map_view = this.options.map_view;
-
-        this.model.bind("change", function(){
-            // alert("change from map_controls");
-        })
-        // this.render();
+        this.model.bind( "change", this.render );
     },
 
     update_filter: function( e )
@@ -24,6 +21,7 @@ snapr.views.map_controls = Backbone.View.extend({
             case 'all':
                 this.model.unset( "username", {silent: true});
                 this.model.unset( "group", {silent: true});
+                this.model.unset( "photo_id", {silent: true});
                 this.model.set({
                     n: 10
                 });
@@ -31,6 +29,7 @@ snapr.views.map_controls = Backbone.View.extend({
             case 'following':
                 this.model.unset( "username", {silent: true});
                 this.model.unset( "group", {silent: true});
+                this.model.unset( "photo_id", {silent: true});
                 this.model.set({
                     group: "following",
                     n: 10
@@ -38,6 +37,7 @@ snapr.views.map_controls = Backbone.View.extend({
                 break;
             case 'just-me':
                 this.model.unset( "group", {silent: true});
+                this.model.unset( "photo_id", {silent: true});
                 this.model.set({
                     username: ".",
                     n: 10
@@ -72,6 +72,33 @@ snapr.views.map_controls = Backbone.View.extend({
     clear_keyword_search: function()
     {
         this.model.unset( "keywords" );
+    },
+
+    render: function()
+    {
+        $(this.el).find("#map-filter option[value='just-me']").attr("disabled", !snapr.auth.has("snapr_user"));
+        $(this.el).find("#map-filter option[value='just-one']").attr("disabled", !this.model.has("photo_id"));
+
+        if (this.model.has( "photo_id" ))
+        {
+            $("#map-filter").val("just-one").selectmenu('refresh', true);
+        }
+        else if (!this.model.has( "username" ) && this.model.get( "group" ) == "following")
+        {
+            $("#map-filter").val("following").selectmenu('refresh', true);
+        }
+        else if (this.model.get( "username" ) == "." && !this.model.has( "group" ))
+        {
+            $("#map-filter").val("just-me").selectmenu('refresh', true);
+        }
+        else
+        {
+            $("#map-filter").val("all").selectmenu('refresh', true);
+        }
+
+        $(this.el).find("#map-keyword input").val( this.model.get("keywords") || "" );
+
+        return this;
     }
 
 });
