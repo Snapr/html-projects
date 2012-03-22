3141c65731d756bdc472e140a7c087e4fc3ac915
David Wolf <dpwolf@mac.com>
2012-03-21 22:13:17 -0400

    keep map thumbs sorted by latitude so they look right

--- app/models/thumb_collection.js
+++ app/models/thumb_collection.js
@@ -1,12 +1,18 @@
 snapr.models.thumb_collection = Backbone.Collection.extend({
-    model:snapr.models.thumb,
-    //urlRoot: snapr.api_base + '/thumbs/',
-    url: function( method ){
+    model: snapr.models.thumb,
+    url: function( method )
+    {
         return snapr.api_base + '/thumbs/';
     },
-    parse: function( d, xhr ){
-        if (d.success && d.response && d.response.photos){
+    parse: function( d, xhr )
+    {
+        if (d.success && d.response && d.response.photos)
+        {
             return d.response.photos;
         }
+    },
+    comparator: function( model )
+    {
+        return - model.get("location").latitude;
     }
 });
