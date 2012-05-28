9cc3df8a5167d4bc3de7b61caa6b7143ab66dd7d
Jake Crosby <jakecrosby@gmail.com>
2012-05-28 14:17:49 +1200

    oops

--- app/views/find_friends.js
+++ app/views/find_friends.js
@@ -10,7 +10,7 @@ snapr.views.find_friends = snapr.views.dialog.extend({
 
         if(this.options.query && this.options.query.back_url){
             var back_url = this.options.query.back_url;
-            this.$('.x-back').attr('href', back_url).attr('data-ajax', false).removeClass('x-back');
+            this.$('.x-back').attr('href', unescape(back_url)).attr('data-ajax', false).removeClass('x-back');
             $.each(this.$('.find-friends-link'), function(){
                 $(this).attr('href', $(this).attr('href') + '?back_url=' + escape(window.location.href));
             });
