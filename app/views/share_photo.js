d369ae02834b3c51655f49ddd0de1239fdfd5b29
David Wolf <dpwolf@mac.com>
2012-06-20 21:01:13 -0400

    default sharing services to off and location sharing to on

--- app/views/share_photo.js
+++ app/views/share_photo.js
@@ -66,10 +66,10 @@ return page_view.extend({
             photo: this.model,
             status: local_storage.get( "status" ),
             share_location: local_storage.get( "share-location" ) !== 'false',
-            facebook_sharing: local_storage.get( "facebook-sharing" ) !== 'false',
-            tumblr_sharing: local_storage.get( "tumblr-sharing" ) !== 'false',
-            foursquare_sharing: local_storage.get( "foursquare-sharing" ) !== 'false',
-            twitter_sharing: local_storage.get( "twitter-sharing" ) !== 'false',
+            facebook_sharing: local_storage.get( "facebook-sharing" ) == 'true',
+            tumblr_sharing: local_storage.get( "tumblr-sharing" ) == 'true',
+            foursquare_sharing: local_storage.get( "foursquare-sharing" ) == 'true',
+            twitter_sharing: local_storage.get( "twitter-sharing" ) == 'true',
             edit: (local_storage.get( "aviary" )  == "true" || local_storage.get( "camplus_edit" )  == "true" ),
             camplus: local_storage.get( "camplus" )  == "true"
         }) ).trigger("create");
