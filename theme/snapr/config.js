e8fc29c2750b5b535147a197f54b615e06fc64c9
Rowan Wernham <rowan@snapr.me>
2012-11-26 11:07:33 +1300

    Set autocreate to true for Facebook sign up

--- theme/snapr/config.js
+++ theme/snapr/config.js
@@ -22,7 +22,7 @@ return {
         anon_username: 'anon',
         me_username: 'me'
 
-        //autocreate_fb_users: false,  // signin with facebook should create users rather than autofill join form
+        autocreate_fb_users: false,  // signin with facebook should create users rather than autofill join form
         //get_user_points: false,  // request points when getting users form the api
         //timeout: 1000*20,  // ms, max api response time allowed
         //offline_timeout: 1000*5,  // ms, max api response time allowed when in offline mode
