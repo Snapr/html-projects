e5c7157ac1ff7e9dcbd4a7b110ffe1893b798638
Rowan Wernham <rowan@snapr.me>
2012-11-28 17:49:27 +1300

    auto create FB users to true

--- theme/snapr/config.js
+++ theme/snapr/config.js
@@ -24,7 +24,7 @@ return {
         anon_username: 'anon',
         me_username: 'me',
 
-        autocreate_fb_users: false // signin with facebook should create users rather than autofill join form
+        autocreate_fb_users: true // signin with facebook should create users rather than autofill join form
         
         //get_user_points: false,  // request points when getting users form the api
         //timeout: 1000*20,  // ms, max api response time allowed
