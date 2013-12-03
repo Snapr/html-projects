fff7eca0e736d725dbc7f61a959a42a495ce0dac
Thomas Collardeau <thomas@collardeau.com>
2013-12-03 21:25:02 +0100

    make tab-bar customizable

--- theme/config.js
+++ theme/config.js
@@ -58,7 +58,7 @@ return {
 
         //!-- Override the app tab bar with a custom template
         //!--default: 'components/tab_bar'
-        //tab_bar_template: theme_templates_path + 'tab_bar',
+        tab_bar_template: theme_templates_path + 'tab_bar',
         //default_tab: 'feed',
 
 
