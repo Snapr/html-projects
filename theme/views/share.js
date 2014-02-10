f875b7519f1bc7e49880dfc2a0a86ab058cab616
Thomas Collardeau <thomas@collardeau.com>
2014-02-10 12:54:59 +0100

    place 'suggest material' button when posting

--- theme/views/share.js
+++ theme/views/share.js
@@ -1,6 +1,16 @@
 define(['views/share', '../../theme/js/material'], function(share_view, material){
     return share_view.extend({
 
+        events: function(){
+              return _.extend({},share_view.prototype.events,{
+                  'change .material-choice' : 'append_selected_tags_to_material_box',
+              });
+           },
+
+        append_selected_tags_to_material_box: function() {
+
+            alert('Coming Soon');
+        }
 
     });
 
