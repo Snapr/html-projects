71dfe2408d306ca8eadc5d113d2dfee13f5d67cb
Thomas Collardeau <thomas@collardeau.com>
2014-02-16 13:05:45 +0100

    make search work (go to photos not feed)

--- theme/views/search.js
+++ theme/views/search.js
@@ -7,6 +7,36 @@ define(['views/search'], function(search_view){
               });
            },
 
+           search: function(e){
+               if(e && e.preventDefault){
+                   e.preventDefault();
+               }
+
+               var keywords = this.$(".x-search-field").val();
+               var type = this.$("select.x-search-type").val();
+
+               switch(type){
+                   case 'location':
+                       if (window.location.hash.indexOf('#/map/') === 0){
+                           this.back();
+                           this.previous_view && this.previous_view.location_search(keywords);
+                       }else{
+                           window.location.hash = "#/map/?location=" + keywords;
+                       }
+                       break;
+                   case 'tag':
+                       if (window.location.hash == "#/photos/?keywords=" + keywords){
+                           this.back();
+                       }else{
+                           window.location.hash = "#/photos/?keywords=" + keywords;
+                       }
+                       break;
+                   case 'user':
+                       dialog('user/search/?username='+keywords);
+                       break;
+               }
+           }
+
     });
 
 });
