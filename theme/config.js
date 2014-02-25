a1a4d171d13547f056bee8e55a2dcdfbbc746fbe
Thomas Collardeau <thomas@collardeau.com>
2014-02-25 16:56:37 +0100

    OVERWRITE tab_bar view

--- theme/config.js
+++ theme/config.js
@@ -65,6 +65,7 @@ return {
         //!-- Override the app tab bar with a custom template
         //!--default: 'components/tab_bar'
         tab_bar_template: theme_templates_path + 'tab_bar',
+        tab_bar_view: '../' + theme_views_path + 'tab_bar',
         default_tab: 'browse',
 
         side_menu_theme: 'c',
