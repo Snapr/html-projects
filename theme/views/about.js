5df355ba6b86f8a3c26fcc809c015aa6fc782e06
Thomas Collardeau <thomas@collardeau.com>
2014-03-06 18:43:36 +0000

    add tab-bar to about page

--- theme/views/about.js
+++ theme/views/about.js
@@ -1,15 +1,24 @@
 define(['views/base/page'], function(about_view){
     return about_view.extend({
 
-        events: function(){
-              return _.extend({},about_view.prototype.events,{
-                    'click .x-menu-button': 'open_menu'
-              });
-           },
+      post_activate: function(){
+          this.change_page();
+          $('.browse-btn').removeClass('ui-btn-active');
+          $('.map-btn').removeClass('ui-btn-active');
 
-          open_menu: function(){
-              $('.x-side-menu').panel('open');
-          }
+          $('.x-side-menu .ui-btn-active').removeClass('ui-btn-active');
+          $('.x-side-menu [data-slug="about"]').addClass('ui-btn-active');
+      },
+
+      events: function(){
+            return _.extend({},about_view.prototype.events,{
+                  'click .x-menu-button': 'open_menu'
+            });
+         },
+
+      open_menu: function(){
+            $('.x-side-menu').panel('open');
+        }
 
     });
 });
